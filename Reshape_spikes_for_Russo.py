2d4cfb784441ec96720c5c60c332a7e12ffb2afb
nielsleadholm <n.leadholm@gmail.com>
2019-01-08 14:40:46 +0000

    Addresses non-spiking neurons and multiple stimuli

--- Reshape_spikes_for_Russo.py
+++ Reshape_spikes_for_Russo.py
@@ -3,59 +3,65 @@
 import numpy as np
 import math
 
-### The following code sorts the output spikes from the Spike simulator into 2D.csv files for use in the Russo algorithm ###
+### The following tool sorts the output spikes from the Spike simulator (1D arrays) into 2D .csv files suitable for analysis by the Russo algorithm ###
 
-#Load .txt files containing neuron id's and spikes
-stim1_ids = np.genfromtxt('output_spikes_posttraining_stim1SpikeIDs.txt')
-stim1_times = np.genfromtxt('output_spikes_posttraining_stim1SpikeTimes.txt')
+#For details on the Russo algorithm, please see the publication "Cell assemblies at multiple time scales with arbitrary lag constellations", Russo and Durstewitz, 2017
+#Spike is a simulator written in C++/CUDA for spiking neural networks, more information on which can be found here: https://sites.google.com/view/spike-simulator/home
+#This code assumes the user is analyzing neural networks wherein each layer is a 2D lattice, and multiple layers are present (see for example the network used in the paper 
+#"The Emergence of Polychronization and Feature Binding in a Spiking Neural Network Model of the Primate Ventral Visual System", Eguchi et al, 2018)
 
-#Specify the number of neurons in each excitatory layer and each inhibitory layer, and number of layers
+#The Russo algorithm requires a 2D array where columns represent neuron IDs, and each neurons first to nth spike are indicated by the column
+#Each array entry specifies the time (in seconds) at which the spike occurred; empty entries are required to be filled with a NaN identifier
+#As the Russo algorithm does not accept neurons that never spike (i.e. empty rows), the following code adds a single spike at a random time-point for such neurons
+
+#The user must provide below parameters that were used in generating the Spike neural network simulation
+#Specify the number of neurons in each excitatory layer and each inhibitory layer, the number of layers, and the number of stimuli (the latter two are both counted from 1)
 excit_dim = 32*32
 inhib_dim = 12*12
 num_layers = 3
-
-#Extract the neurons of interest
 extracted_layer = 3 #Identify the neuron layer of interest (e.g. 3rd layer)
-extracted_mask = np.where((excit_dim*(extracted_layer-1) < stim1_ids) & (stim1_ids <= extracted_layer*excit_dim)) #Returns an array of indeces for the neurons in the layer of interest
+num_stimuli = 2
 
-extracted_ids = np.take(stim1_ids, extracted_mask) #Returns an array of spike IDs, restricted to the layer of interest
-extracted_times = np.take(stim1_times, extracted_mask) #Returns an array of spike times, restricted to the layer of interest
 
-extracted_ids = np.reshape(extracted_ids, len(extracted_ids[0])) #Re-shape the array into a column array so that can be used by np.bincount later
-extracted_ids = extracted_ids.astype(int) #Convert from spike output (float)
+#Load .txt files containing neuron id's and spikes
+for jj in range(0, num_stimuli):
+	stim_ids = np.genfromtxt('output_spikes_posttraining_stim' + str(jj+1) +'SpikeIDs.txt')
+	stim_times = np.genfromtxt('output_spikes_posttraining_stim1SpikeTimes.txt')
 
-#Identify the neuron that spikes the max number of times, and return the number of times it spikes
-max_spikes = np.max(np.bincount(extracted_ids))
+	#Extract the neurons of interest
+	extracted_layer = 3 #Identify the neuron layer of interest (e.g. 3rd layer)
+	extracted_mask = np.where((excit_dim*(extracted_layer-1) < stim_ids) & (stim_ids <= extracted_layer*excit_dim)) #Returns an array of indeces for the neurons in the layer of interest
 
-#Initialize a NaN array with rows = number of unique neurons in the layer, and columns = number of spikes of the maximally active neuron
-Russo_array = np.zeros([excit_dim, max_spikes])
-Russo_array[:, :] = np.nan
+	extracted_ids = np.take(stim_ids, extracted_mask) #Returns an array of spike IDs, restricted to the layer of interest
+	extracted_times = np.take(stim_times, extracted_mask) #Returns an array of spike times, restricted to the layer of interest
 
-print(max_spikes)
-print(np.max(extracted_times))
+	extracted_ids = np.reshape(extracted_ids, len(extracted_ids[0])) #Re-shape the array into a column array so that it can be used by np.bincount later
+	extracted_ids = extracted_ids.astype(int) #Convert from spike output (float)
 
-#Iterate through each neuron of interest
+	#Identify the neuron that spikes the max number of times, and return the number of times it spikes, used later
+	max_spikes = np.max(np.bincount(extracted_ids))
 
-for ii in range(0, len(Russo_array[:, 0])):
-  #Extract a binary mask containing the indeces of when the neuron of interest has fired
-  temp_mask = np.where(extracted_ids == (excit_dim*(extracted_layer - 1) + ii + 1))
-  #Use the mask to identify all the spike times associated with that neuron, and assign it to the 'Russo_array'
-  Russo_array[ii, 0:(np.size(np.take(extracted_times, temp_mask)))] = np.take(extracted_times, temp_mask)
+	#Initialize a NaN array with rows = number of unique neurons in the layer, and columns = number of spikes of the maximally active neuron
+	Russo_array = np.zeros([excit_dim, max_spikes])
+	Russo_array[:, :] = np.nan
 
-#The Russo algorithm throws an error if a row contains no spikes, therefore condensed_array removes these
-condensed_array = np.zeros([np.sum(Russo_array[:, 0] > 0), max_spikes])
-condensed_array[:, :] = np.nan
+	# print(max_spikes)
+	# print(np.max(extracted_times))
 
-temp_counter = 0
-for ii in range(0, len(Russo_array[:, 0])):
-  if math.isnan(Russo_array[ii, 0]) == 0: #Checks if the first element is NaN, in which case the entire row is; if not, then fills condensed_array with that row
-    condensed_array[temp_counter, :] = Russo_array[ii, :]
-    temp_counter += 1
+	#Iterate through each neuron of interest, inserting its spikes into the Russo-suitable array; if a neuron never spikes, insert a single random spike
+	for ii in range(0, len(Russo_array[:, 0])):
+	  #Extract a binary mask containing the indeces of when the neuron of interest has fired
+	  temp_mask = np.where(extracted_ids == (excit_dim*(extracted_layer - 1) + ii + 1))
+	  #Use the mask to identify all the spike times associated with that neuron, and assign it to the 'Russo_array'
+	  Russo_array[ii, 0:(np.size(np.take(extracted_times, temp_mask)))] = np.take(extracted_times, temp_mask)
+	  if math.isnan(Russo_array[ii, 0]) == 1: #Checks if the first element is NaN, in which case the entire row is (i.e. the neuron never spiked)
+	    Russo_array[ii, 0] = np.random.random()*np.max(extracted_times) #Assigns the neuron a single spike, time of which is sampled from a continuous uniform distribution
 
-print(condensed_array[0:10, 0:10])
+	#print(Russo_array[0:20, 0:5])
+	#print(np.shape(Russo_array))
 
-#Output file as CSV
-#np.savetxt("Russo_array_stim1.csv", condensed_array, delimiter=',')
+	#Output file as CSV
+	np.savetxt("posttraining_stim" + str(jj+1) + "_Russo.csv", Russo_array, delimiter=',')
 
 
 
